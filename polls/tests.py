dae3bc94517b9c71e4db773ecad1402cebe05806
simar <mehtasimar2015@gmail.com>
2024-10-16 02:23:18 -0400

    CI/CD changes 2

---
+++ polls/tests.py
@@ -0,0 +1,14 @@
+# polls/tests.py
+
+from django.test import TestCase
+from .models import Poll
+from django.utils import timezone
+
+class PollModelTest(TestCase):
+
+    def test_poll_creation(self):
+        """
+        Test that a poll is created successfully.
+        """
+        poll = Poll.objects.create(question="Sample question?", pub_date=timezone.now())
+        self.assertEqual(poll.question, "Sample question?")
